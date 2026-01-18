fb690196b878812eb126ee94311b847eee24cb96
shooterop357-rgb <shooterop357@gmail.com>
2026-01-18 08:22:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ import subprocess
 import datetime
 import os
 
-from keep_alive import keep_alive
-keep_alive()
 # insert your Telegram bot token here
 bot = telebot.TeleBot('8583192474:AAHP1s3tQs0e8sKSObvYHUYqBeJLRvRaQ28')
 
@@ -451,5 +449,6 @@ while True:
         bot.polling(none_stop=True)
     except Exception as e:
         print(e)
+
 
 
